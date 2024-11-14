1ab9f813ec09b318f22c8a8509cb95bc537848bd
Raed Jarboui <raedjarboui1998@gmail.com>
2024-11-14 16:43:00 +0100

    add resilience4j circuitbreakerConfiguration

--- user-service/src/main/java/com/user/resilience4j/dto/OrderDTO.java
+++ user-service/src/main/java/com/user/resilience4j/dto/OrderDTO.java
@@ -8,11 +8,48 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class OrderDTO {
-    private int id;
     private String name;
     private String category;
     private String color;
-    private double price;
+    private int price;
+    
+    public OrderDTO() {
+    	
+    }
+    
+	public OrderDTO(String name, String category, String color, int price) {
+		super();
+		this.name = name;
+		this.category = category;
+		this.color = color;
+		this.price = price;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+    
+    
 }
 		
 		
--- user-service/src/main/java/com/user/resilience4j/endpoint/UserController.java
+++ user-service/src/main/java/com/user/resilience4j/endpoint/UserController.java
@@ -1,6 +1,10 @@
 package com.user.resilience4j.endpoint;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -12,10 +16,14 @@ import org.springframework.web.client.RestTemplate;
 
 import com.user.resilience4j.dto.OrderDTO;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+
 @RestController
 public class UserController {
 	
 	private final String BASE_URL = "http://localhost:8081/orders";
+	
+	public static final String  USER_SERVICE="userService";
 	
 	 @Autowired
 	    @Lazy
@@ -27,12 +35,24 @@ public class UserController {
 	}
 	
 	@SuppressWarnings("unchecked")
+	@CircuitBreaker(name=USER_SERVICE,fallbackMethod = "getDefaultOrders")
 	@GetMapping("/getOrders")
 	public List<OrderDTO> getOrders(){
 		return (List<OrderDTO>) restTemplate.getForObject(BASE_URL + "/list", Object.class);
 		
 	
 	}
+	
+	 public List<OrderDTO> getDefaultOrders(Exception e){
+	        return List.of(
+	                new OrderDTO("LED TV", "electronics", "white", 45000),
+	                new OrderDTO("Headset", "electronics", "black", 7000),
+	                new OrderDTO("Sound bar", "electronics", "black", 13000),
+	                new OrderDTO("Puma Shoes", "foot wear", "black & white", 4600),
+	                new OrderDTO("Vegetable chopper", "kitchen", "blue", 999),
+	                new OrderDTO("Oven Gloves", "kitchen", "gray", 745)
+	        );
+	    }
 	
 	@GetMapping("/getOrders/{category}")
 	public List<OrderDTO> getOrdersByCategory(@PathVariable String category){
